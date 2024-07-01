7889495aa3d647d3a4920a733817a2528c367da7
Shaguftaseraj <ahmadshagufta229@gmail.com>
2024-07-01 20:55:36 +0600

    3rd.sol

--- 3rd.sol
+++ 3rd.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DegenGamingToken is ERC20, Ownable {
+    constructor() ERC20("Degen Gaming Token", "DGT") Ownable(msg.sender) {
+        // Mint initial supply to the contract deployer
+        _mint(msg.sender, 1000000 * 10 ** decimals()); 
+    }
+
+    function mint(address to, uint256 balance ) public onlyOwner {
+        _mint(to, balance);
+    }
+
+    function burn(uint256 balance) public {
+        _burn(msg.sender, balance);
+    }
+}
 
