08263895ed67a8e8f87c51dc32fcf527b5547d42
Shaguftaseraj <ahmadshagufta229@gmail.com>
2024-07-01 20:59:16 +0600

    3rd.sol

--- 3rd.sol
+++ 3rd.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenGamingToken is ERC20, Ownable {
-    constructor() ERC20("Degen Gaming Token", "DGT") Ownable(msg.sender) {
+contract DGToken is ERC20, Ownable {
+    constructor() ERC20("DG. Token", "DGT") Ownable(msg.sender) {
         // Mint initial supply to the contract deployer
         _mint(msg.sender, 1000000 * 10 ** decimals()); 
     }
